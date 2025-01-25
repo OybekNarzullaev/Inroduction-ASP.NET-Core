ea1eb84dd0e35df046f062cf723a64166cb64fd6
OybekNarzullaev <oybeknarzullaev99@gmail.com>
2025-01-25 23:46:35 +0500

    OnlineMarket

---
+++ OnlineMarket/Models/ViewModels/Register.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineMarket.Models;
+
+public class RegisterViewModel
+{
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    public DateTime DateOfBirth { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    [Required]
+    public string ConfirmPassword { get; set; }
+}
---
+++ OnlineMarket/Models/ViewModels/UserRole.cs
@@ -0,0 +1,10 @@
+namespace OnlineMarket.Models;
+public class UserRoleViewModel
+{
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public List<string> Roles { get; set; }
+}
