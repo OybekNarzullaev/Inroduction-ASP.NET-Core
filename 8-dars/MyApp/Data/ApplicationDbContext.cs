05ebfe60cf19c34202a6bc47dd2974ffe63d833a
OybekNarzullaev <oybeknarzullaev99@gmail.com>
2025-02-01 22:26:46 +0500

    8-dars

---
+++ 8-dars/MyApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MyApp.Models;
+
+namespace MyApp.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+    {
+    }
+    public DbSet<Product> Products { get; set; }
+}
